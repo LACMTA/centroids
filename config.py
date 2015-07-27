b4da29c36693268ddc841c2575a216574e6500f7
Douglas Goodwin <dgoodwin@gmail.com>
2015-07-27 09:37:20 +0200

    debug and DEBUG

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
-DEBUG = True
+DEBUG = debug = True
 
 # Secret key for session management. You can generate random strings here:
 # http://clsc.net/tools-old/random-string-generator.php
@@ -15,7 +15,6 @@ SECRET_KEY = 'alsdlkjasd0897sfdg45sfdgfdsaqzdf98sdf0a'
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
-SQLALCHEMY_POOL_RECYCLE = 3600
-
+# SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
